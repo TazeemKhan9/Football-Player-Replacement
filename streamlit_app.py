0c632a48a8074bc36d6b88a4c0f790cc474eea79
Tazeem Khan <67814275+TazeemKhan9@users.noreply.github.com>
2020-10-20 09:35:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -461,7 +461,7 @@ def main():
 		
 		
 		val=num
-		Age=st.number_input(label="Enter age",format="%i",step=1)
+		Age=st.number_input(label="Enter age",format="%i",value=0,step=1)
 		if user_input=='GK':
 			gk_search=load_data(url1,1000)
 			gk_advance=load_data(url2,1000)
